623d685d732eafb643f5375ba4e2417a0abdb235
wp-json <168683856+mixcine@users.noreply.github.com>
2024-07-14 02:44:40 -0300

    Delete mysql/api.php

--- mysql/api.php
+++
@@ -1,1 +0,0 @@
-{"870358":"{\"ids\":\"870358\",\"logos\":\"false\",\"poster\":\"t2wpjvGFA9ZdQFZn4Phg6QKZRBH.jpg\",\"banner\":iiojh"\",\"name\":\"O Natal do Pequeno Batman\",\"genres\":\"Animação, Ação, Família, Comédia\",\"ranking\":\"false\",\"pagina\":\"false\",\"date\":\"14072024 02:38\",\"type\":\"filmes\"}"}
