90d127a6429ea254321e34076e59feff7a937f49
wp-json <168683856+mixcine@users.noreply.github.com>
2024-07-14 01:52:47 -0300

    movie

---
+++ mysql/api.php
@@ -0,0 +1,1 @@
+{"414906":"{\"ids\":\"414906\",\"poster\":\"/wd7b4Nv9QBHDTIjc2m7sr0IUMoh.jpg\",\"banner\":\"/tRS6jvPM9qPrrnx2KRp3ew96Yot.jpg\",\"name\":\"Batman\",\"genres\":\"Crime, Mistério, Thriller\",\"logos\":\"\",\"ranking\":\"n/a\",\"pagina\":\"false\",\"date\":\"14/07/2024 01:52\",\"type\":\"filmes\"}"}
